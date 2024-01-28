4add5420d9b5cfe3d6fbbf0067d47d66a4bcd5ad
NikitaZaver <zavershinskiy.nik@yandex.ru>
2024-01-28 15:57:39 +0700

    Fix average2

--- src/Average.java
+++ src/Average.java
@@ -1,24 +1,16 @@
 public class Average {
-
     public static void main(String[] args) {
+        int kol = 0;
+        int sum = 0;
         int a = 3;
         int b = 17;
-        int c = b - a + 1;
-        int sum = 0;
 
-        do {
-            sum += a;
-            ++a;
-        } while (a<=b);
-
-        System.out.print("Среднее арифметическое чисел равно: " + (double) sum/c);
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                kol++;
+                sum += i;
+            }
+        }
+        System.out.println("Среднее арифметическое четных чисел: " + (double) sum / kol);
     }
-
-
-
-
-
 }
-
-
-
