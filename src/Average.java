a345339504340d12925c76f10e6b16e25bf36b74
NikitaZaver <zavershinskiy.nik@yandex.ru>
2024-01-28 13:19:42 +0700

    Initial commit

---
+++ src/Average.java
@@ -0,0 +1,17 @@
+public class Average {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 17;
+        int sum = 3;
+
+        while (a<=b){
+            sum = sum + a;
+            ++a;
+        }
+        
+
+
+
+    }
+}
