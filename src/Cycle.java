cce56f641e96963f56bf04134394fcec8e481ed5
NikitaZaver <zavershinskiy.nik@yandex.ru>
2024-01-30 17:12:40 +0700

    Add Cycle.java and development

---
+++ src/Cycle.java
@@ -0,0 +1,23 @@
+public class Cycle {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >= 1; i--) {
+            int Multiplicity = getMultiplicity(i);
+            if (Multiplicity == 0) {
+                System.out.print(i);
+            }
+        }
+    }
+
+    public static int getMultiplicity(int i) {
+        return i % 4;
+
+    }
+}
+
+
+
+
+
+
+
