bf051ccde26752526328d0bcd2978a7ba7144ddd
NikitaZaver <zavershinskiy.nik@yandex.ru>
2024-01-31 13:57:32 +0700

    Fix Average.java

--- src/Average.java
+++ src/Average.java
@@ -6,8 +6,8 @@ public class Average {
 
         int quantity = 0;
         int sum = 0;
-        int quantity1 = 0;
-        int sum1 = 0;
+        int quantityEven = 0;
+        int sumEven = 0;
 
         System.out.print("Введите min число диапазона: ");
         int min = scanner.nextInt();
@@ -18,15 +18,12 @@ public class Average {
         for (int i = min; i <= max; i++) {
             quantity++;
             sum += i;
-        }
-        System.out.println("Среднее арифметическое чисел: " + (double) sum / quantity);
-
-        for (int j = min; j <= max; j++) {
-            if (j % 2 == 0) {
-                quantity1++;
-                sum1 += j;
+            if (i % 2 == 0) {
+                quantityEven++;
+                sumEven += i;
             }
         }
-        System.out.println("Среднее арифметическое чётных чисел: " + (double) sum1 / quantity1);
+        System.out.println("Среднее арифметическое чисел: " + (double) sum / quantity);
+        System.out.println("Среднее арифметическое чётных чисел: " + (double) sumEven / quantityEven);
     }
 }
