7deb67b20eb20763f8b00136ad7f09230ad522ef
NikitaZaver <zavershinskiy.nik@yandex.ru>
2024-02-01 14:36:45 +0700

    Add Numbers.java and development

---
+++ src/Numbers.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int sumNotEven = 0;
+        int maxNumber = 0;
+
+        System.out.print("Введите целое число: ");
+        int number = scanner.nextInt();
+
+        for (int i = number; i != 0; i /= 10) {
+            sum += i % 10;
+            if (i % 2 != 0) {
+                sumNotEven += i % 10;
+            }
+            if (maxNumber < (i % 10)){
+                maxNumber = i % 10;
+            }
+        }
+        System.out.println("Сумма всех цифр = " + sum);
+        System.out.println("Сумма всех нечётных цифр = " + sumNotEven);
+        System.out.println("Максимальная цифра = " + maxNumber);
+    }
+}
+
+
+
